81ed4ba79941a0dd3bc23c7ab704851a63f54838
Tommy Sprague <tommy.sprague@gmail.com>
2020-12-10 18:01:14 -0800

    plot eccentricity changes, and add mode for generating noise in polar coords

--- vRF_simChanges.m
+++ vRF_simChanges.m
@@ -16,11 +16,9 @@
 % - -- scheme 2: use one random noise condition to sort, look at changes
 %      ("incorrect")
 %
-% TODO: rewrite so that uniform is a special case of linear slope...
-% TODO: ecc > ecc_thresh, scale vectors to that length (so ecc == 15-eps)
-% TODO: ecc plots (not just size)
 % TODO: (related to above) separate out computing binned values and
 %       plotting...
+% TODO: fix noise scaling for polar angle...
 %
 % TC Sprague 12/9/2020
 
@@ -51,43 +49,25 @@ rf_baseline = [rf.x0(goodvox);rf.y0(goodvox);rf.sigma(goodvox)];
 % number of simulation iterations
 n_iter = 50;
 
-noise_mode = 2; % 1 = uniform, 2 = scales w/ ecc
 
-% MODE 1: uniform noise added to x, y, sigma of different magnitudes
+% additive gaussian noise w/ std dev = const + slope*ecc
+% Dumoulin & Wandell 2008 - .0625 size vs ecc slope in V1 for 1 subj
+lin_noise_const = 0.1*[0.25 0.25 0.2;    1.5  1.5  0.5];
+lin_noise_slope = 5*[0.1  0.1  0.0625; 0.25 0.25 0.15]; % set these to 0 for uniform noise
+
+% cell array: for each noise level (rows above), n_params (3) x n_vox x n_iter - each parameter randomly wiggled
+rf_noisesim = cell(size(lin_noise_const,1),1);
+
+tmp_ecc = sqrt(sum(rf_baseline([1 2],:).^2,1));
+
+
+
+noise_mode = 2; % 1 = uniform x/y, 2 = uniform ecc/pol
+
+% MODE 1: additive noise in x,y, no bias (either uniform, set slope to 0, or scales w/ ecc)
 
 if noise_mode == 1
-    uni_noise_mag = 5*[0.5 0.5 0.25; 1.5 1.5 0.75]; % add Gaussian noise to each vox for x, y, sigma (in deg vis angle); each row is a different noise level value
     
-    
-    % cell array: for each noise level (rows above), n_params (3) x n_vox x n_iter - each parameter randomly wiggled
-    rf_noisesim = cell(size(uni_noise_mag,1),1);
-    for nn = 1:size(uni_noise_mag,1)
-        rf_noisesim{nn} = nan(size(rf_baseline,1),size(rf_baseline,2),n_iter);
-        for ii = 1:n_iter
-            rf_noisesim{nn}(:,:,ii) = rf_baseline + randn(size(rf_baseline)) .* uni_noise_mag(nn,:).';
-            rf_noisesim{nn}(3,rf_noisesim{nn}(3,:,ii)<0,ii) = 0.01;
-        end
-        
-    end
-    
-
-
-
-
-
-
-% MODE 2: noise scales with eccentricity
-elseif noise_mode == 2
-
-    % additive gaussian noise w/ std dev = const + slope*ecc
-    % Dumoulin & Wandell 2008 - .0625 size vs ecc slope in V1
-    lin_noise_const = 2*[0.25 0.25 0.2;    1.5  1.5  0.5];  
-    lin_noise_slope = 5*[0.1  0.1  0.0625; 0.25 0.25 0.15];
-
-    % cell array: for each noise level (rows above), n_params (3) x n_vox x n_iter - each parameter randomly wiggled
-    rf_noisesim = cell(size(lin_noise_const,1),1);
-
-    tmp_ecc = sqrt(sum(rf_baseline([1 2],:).^2,1));
     
     for nn = 1:length(rf_noisesim)
         
@@ -104,31 +84,50 @@ elseif noise_mode == 2
         tmpxy  = rf_noisesim{nn}([1 2],:,:);
         tmpecc = sqrt(sum(rf_noisesim{nn}([1 2],:,:).^2,1)); % 1 x nvox x niter
         % those that are >= ecc_thresh, compute a scale factor
-        bigecc = tmpecc>ecc_thresh;
+        bigecc = tmpecc>=ecc_thresh;
         tmpscale = tmpecc(bigecc)/ecc_thresh; % divide by tmpscale
         
         all_scale = ones(size(tmpecc));
-        all_scale(bigecc) = 1./tmpscale;
+        all_scale(bigecc) = 1./(tmpscale+.001); % to keep things below 15...
         all_scale = repmat(all_scale,2,1,1);
         
         rf_noisesim{nn}([1 2],:,:) = tmpxy.*all_scale;
         
     end
-    
 
+% MODE 2: additive noise in ecc, pol    
+elseif noise_mode == 2
 
-
-
-% MODE 3: noise depends on hemisphere [[maybe this is a different scenario
-% that deserves a different script...?]]
-elseif noise_mode == 3 % TODO
-
-
-
-
-
-
-
+    for nn = 1:length(rf_noisesim)
+        
+        this_noise_scale = lin_noise_const(nn,:).' + lin_noise_slope(nn,:).' .* tmp_ecc;
+        
+        rf_noisesim{nn} = nan(size(rf_baseline,1),size(rf_baseline,2),n_iter);
+        
+        [tmpth,tmprad] = cart2pol(rf_baseline(1,:),rf_baseline(2,:));
+        tmppol = [tmpth;tmprad];
+        tmppol = tmppol + randn([2 size(this_noise_scale,2) n_iter]) .* this_noise_scale([1 2],:);
+        [tmpx,tmpy] = pol2cart(tmppol(1,:,:),tmppol(2,:,:));
+        rf_noisesim{nn}([1 2],:,:) = [tmpx;tmpy]; clear tmpx tmpy tmppol tmpth tmprad;
+        rf_noisesim{nn}(3,:,:) = rf_baseline(3,:) + randn([1 size(this_noise_scale,2) n_iter]) .* this_noise_scale(3,:);
+        tmpsig = rf_noisesim{nn}(3,:,:);
+        tmpsig(tmpsig<0) = 0.1;
+        rf_noisesim{nn}(3,:,:) = tmpsig; clear tmpsig;
+        
+        % rescale ecc to be within ecc_thresh (like vRF param restrictions)
+        tmpxy  = rf_noisesim{nn}([1 2],:,:);
+        tmpecc = sqrt(sum(rf_noisesim{nn}([1 2],:,:).^2,1)); % 1 x nvox x niter
+        % those that are >= ecc_thresh, compute a scale factor
+        bigecc = tmpecc>=ecc_thresh;
+        tmpscale = tmpecc(bigecc)/ecc_thresh; % divide by tmpscale
+        
+        all_scale = ones(size(tmpecc));
+        all_scale(bigecc) = 1./(tmpscale+.001); % to keep things below 15...
+        all_scale = repmat(all_scale,2,1,1);
+        
+        rf_noisesim{nn}([1 2],:,:) = tmpxy.*all_scale;
+        
+    end
 
 end
 
@@ -141,7 +140,7 @@ end
 
 
 
-% PLOT EVERYTHING
+%% PLOT EVERYTHING - SIZE VS ECC
 
 % first - scatterplot of size vs ecc with overlaid bins
 % (one subplot per noise level; sorted within simulation) - just the first
@@ -254,7 +253,7 @@ for ii = 1:n_iter
     for nn = 1:length(rf_noisesim)
         for bb = 1:length(ecc_bin_centers)
             thisidx = this_ecc_bin_idx==bb;
-            binned_sig_byonecond(nn,bb,:) = mean(rf_noisesim{nn}(3,thisidx,:),2);
+            binned_sig_byonecond(nn,bb,ii) = mean(rf_noisesim{nn}(3,thisidx,ii),2);
             clear thisidx;
         end
     end
@@ -303,7 +302,7 @@ for ii = 1:n_iter
         
         for bb = 1:length(ecc_bin_centers)
             thisidx = this_ecc_bin_idx==bb;
-            binned_sig_byeachcond(nn,bb,:) = mean(rf_noisesim{nn}(3,thisidx,:),2);
+            binned_sig_byeachcond(nn,bb,ii) = mean(rf_noisesim{nn}(3,thisidx,ii),2);
             clear thisidx;
         end
         clear tmp_ecc this_ecc_bin_idx;
@@ -329,11 +328,152 @@ thism = mean(binned_sig_byeachcond(end,:,:),3) - mean(binned_sig_byeachcond(1,:,
 thisci = squeeze(prctile(binned_sig_byeachcond(end,:,:) - binned_sig_byeachcond(1,:,:),[2.5 97.5],3));
 plot(ecc_bin_centers.*[1;1],thisci.','k-','LineWidth',1.5);
 plot(ecc_bin_centers,thism,'ko-','MarkerSize',7,'LineWidth',1.5,'MarkerFaceColor','w');
-xlabel('Eccentricity (Each condition 1; \circ)'); ylabel('\Delta size (\circ)');
+xlabel('Eccentricity (Each condition; \circ)'); ylabel('\Delta size (\circ)');
 title('Binned based on each condition ecc');
 set(gca,'TickDir','out');
 
 
 sgtitle('Size changes, binned by ecc');
+match_ylim(absax);
+match_ylim(diffax);
+
+%% PLOT EVERYTHING: delta ECC vs ECC
+%
+% bin based on:
+% - col 1: baseline ecc
+% - col 2: cond 1 ecc
+% - col 3: bin based on each cond ecc
+
+figure;
+
+
+% compare binned size vs ecc, binning using baseline
+absax(1) = subplot(2,3,1); hold on;
+binned_ecc_bybaseline = nan(length(rf_noisesim),length(baseline_ecc_bin_centers),n_iter);
+for nn = 1:length(rf_noisesim)
+    for bb = 1:length(baseline_ecc_bin_centers)       
+        thisidx = baseline_ecc_bin_idx==bb;
+        binned_ecc_bybaseline(nn,bb,:) = mean(sqrt(sum(rf_noisesim{nn}([1 2],thisidx,:).^2,1)),2);
+    end
+    thism = mean(binned_ecc_bybaseline(nn,:,:),3);
+    thisci = squeeze(prctile(binned_ecc_bybaseline(nn,:,:),[2.5 97.5],3)); % n_bins x 2
+    plot(baseline_ecc_bin_centers.*[1;1],thisci.','-','LineWidth',1.5,'Color',noise_colors(nn,:));
+    plot(baseline_ecc_bin_centers,thism,'o-','MarkerSize',7,'LineWidth',1.5,'Color',noise_colors(nn,:),'MarkerFaceColor','w');
+    clear thism thisci;
+end
+
+xlabel('Eccentricity (baseline; \circ)'); ylabel('Eccentricity (\circ)');
+title('Binned based on baseline eccentricity');
+set(gca,'TickDir','out');
+
+% difference (end - 1 in case there are > 2 noise values used)
+diffax(1) = subplot(2,3,4); hold on;
+
+thism = mean(binned_ecc_bybaseline(end,:,:),3) - mean(binned_ecc_bybaseline(1,:,:),3);
+thisci = squeeze(prctile(binned_ecc_bybaseline(end,:,:)-binned_ecc_bybaseline(1,:,:),[2.5 97.5],3)); % n_bins x 2
+
+plot(baseline_ecc_bin_centers.*[1;1],thisci.','-','LineWidth',1.5,'Color','k');
+plot(baseline_ecc_bin_centers,thism,'ko-','MarkerSize',7,'LineWidth',1.5,'MarkerFaceColor','w');
+xlabel('Eccentricity (baseline; \circ)'); ylabel('\Delta eccentricity (\circ)');
+title('Binned based on baseline eccentricity');
+set(gca,'TickDir','out');
+clear thisci thism;
+
+
+
+% compare binned ecc vs ecc, binning using one condition (row 1)
+absax(2) = subplot(2,3,2); hold on;
+binned_ecc_byonecond = nan(length(rf_noisesim),length(baseline_ecc_bin_centers),n_iter);
+
+% do the binning per iteration
+for ii = 1:n_iter
+    
+    tmp_ecc = sqrt(sum(rf_noisesim{1}([1 2],:,ii).^2,1)); % TODO - need to recompute on each iter...    
+    [this_ecc_bin_idx] = discretize(tmp_ecc,0:1:ecc_thresh);
+
+    for nn = 1:length(rf_noisesim)
+        for bb = 1:length(ecc_bin_centers)
+            thisidx = this_ecc_bin_idx==bb;
+            binned_ecc_byonecond(nn,bb,ii) = mean(sqrt(sum(rf_noisesim{nn}([1 2],thisidx,ii).^2,1)),2);
+            clear thisidx;
+        end
+    end
+    clear tmp_ecc this_ecc_bin_idx;
+end
+
+% plot
+for nn = 1:length(rf_noisesim)
+    thism  = mean(binned_ecc_byonecond(nn,:,:),3);
+    thisci = squeeze(prctile(binned_ecc_byonecond(nn,:,:),[2.5 97.5],3));
+    plot(ecc_bin_centers.*[1;1],thisci.','-','Color',noise_colors(nn,:),'LineWidth',1.5);
+    plot(ecc_bin_centers,thism,'o-','MarkerSize',7,'LineWidth',1.5,'Color',noise_colors(nn,:),'MarkerFaceColor','w');
+    clear thism thisci;
+end
+xlabel('Eccentricity (Cond 1; \circ)'); ylabel('Eccentricity (\circ)');
+title('Binned based on single condition ecc');
+set(gca,'TickDir','out');
+
+% difference (end - 1 in case there are > 2 noise values used)
+diffax(2) = subplot(2,3,5); hold on;
+
+thism  = mean(binned_ecc_byonecond(end,:,:),3) - mean(binned_ecc_byonecond(1,:,:),3);
+thisci = squeeze(prctile(binned_ecc_byonecond(end,:,:) - binned_ecc_byonecond(1,:,:),[2.5 97.5],3));
+
+plot(ecc_bin_centers.*[1;1],thisci.','k-','LineWidth',1.5);
+plot(ecc_bin_centers,thism,'ko-','MarkerSize',7,'LineWidth',1.5,'MarkerFaceColor','w');
+clear thism thisci;
+
+xlabel('Eccentricity (Cond 1; \circ)'); ylabel('\Delta eccentricity (\circ)');
+title('Binned based on single condition ecc');
+set(gca,'TickDir','out');
+
+% compare binned size vs ecc, binning using each condition 
+absax(3) = subplot(2,3,3); hold on;
+binned_ecc_byeachcond = nan(length(rf_noisesim),length(baseline_ecc_bin_centers),n_iter);
+
+% do the binning per iteration
+for ii = 1:n_iter
+    
+
+    for nn = 1:length(rf_noisesim)
+        
+        tmp_ecc = sqrt(sum(rf_noisesim{nn}([1 2],:,ii).^2,1)); % TODO - need to recompute on each iter...
+        [this_ecc_bin_idx] = discretize(tmp_ecc,0:1:ecc_thresh);
+
+        
+        for bb = 1:length(ecc_bin_centers)
+            thisidx = this_ecc_bin_idx==bb;
+            binned_ecc_byeachcond(nn,bb,ii) = mean(sqrt(sum(rf_noisesim{nn}([1 2],thisidx,ii).^2,1)),2);
+            clear thisidx;
+        end
+        clear tmp_ecc this_ecc_bin_idx;
+    end
+    
+end
+
+% plot
+for nn = 1:length(rf_noisesim)
+    thism  = mean(binned_ecc_byeachcond(nn,:,:),3);
+    thisci = squeeze(prctile(binned_ecc_byeachcond(nn,:,:),[2.5 97.5],3));
+    plot(ecc_bin_centers.*[1;1],thisci.','-','LineWidth',1.5,'Color',noise_colors(nn,:));
+    plot(ecc_bin_centers,thism,'o-','MarkerSize',7,'LineWidth',1.5,'Color',noise_colors(nn,:),'MarkerFaceColor','w');
+    clear thism thisci;
+end
+xlabel('Eccentricity (each condition); \circ)'); ylabel('Eccentricity (\circ)');
+title('Binned based on each condition ecc');
+set(gca,'TickDir','out');
+
+% difference (end - 1 in case there are > 2 noise values used)
+diffax(3) = subplot(2,3,6); hold on;
+thism = mean(binned_ecc_byeachcond(end,:,:),3) - mean(binned_ecc_byeachcond(1,:,:),3);
+thisci = squeeze(prctile(binned_ecc_byeachcond(end,:,:) - binned_ecc_byeachcond(1,:,:),[2.5 97.5],3));
+plot(ecc_bin_centers.*[1;1],thisci.','k-','LineWidth',1.5);
+plot(ecc_bin_centers,thism,'ko-','MarkerSize',7,'LineWidth',1.5,'MarkerFaceColor','w');
+xlabel('Eccentricity (Each condition; \circ)'); ylabel('\Delta eccentricity (\circ)');
+title('Binned based on each condition ecc');
+set(gca,'TickDir','out');
+
+
+sgtitle('Ecc changes, binned by ecc');
 match_ylim(absax);
 match_ylim(diffax);
